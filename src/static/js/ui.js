cd7c6f6e34b00900678578220cf8be4958e8809e
József Mandula <63676153+joealmond@users.noreply.github.com>
2022-02-09 21:52:20 +0100

    mobil menu fix

--- src/static/js/ui.js
+++ src/static/js/ui.js
@@ -1,5 +1,13 @@
 const navList = document.querySelector(".nav__list");
+const navListItem = document.querySelectorAll(".nav__list-item");
 const navButton = document.querySelector(".nav__button");
 navButton.addEventListener("click", () => {
+  const index = { i: 10 };
+  navListItem.forEach((element) => {
+    index.i = index.i + 10;
+    setTimeout(() => {
+      element.classList.toggle("show");
+    }, index.i);
+  });
   navList.classList.toggle("show");
 });
