2f4f5b3e48bdcdaad23aaed4a8b10709114d24aa
József Mandula <63676153+joealmond@users.noreply.github.com>
2022-02-09 22:02:56 +0100

    hide mobil menu on scroll

--- src/static/js/ui.js
+++ src/static/js/ui.js
@@ -1,6 +1,7 @@
 const navList = document.querySelector(".nav__list");
 const navListItem = document.querySelectorAll(".nav__list-item");
 const navButton = document.querySelector(".nav__button");
+
 navButton.addEventListener("click", () => {
   const index = { i: 10 };
   navListItem.forEach((element) => {
@@ -10,4 +11,11 @@ navButton.addEventListener("click", () => {
     }, index.i);
   });
   navList.classList.toggle("show");
+});
+
+document.addEventListener("scroll", function (e) {
+  navListItem.forEach((element) => {
+    element.classList.remove("show");
+  });
+  navList.classList.remove("show");
 });
